4f3b3bf6b589f4a862a99ee8dae0078d6d64f5b9
Mario <icutum@hotmail.com>
2022-09-30 18:25:56 +0200

    Ejercicio del enunciado finalizado

--- Tema 2/enunciado/enunciado.php
+++ Tema 2/enunciado/enunciado.php
@@ -26,50 +26,54 @@
                     $precioProducto = $productos[$nombreProducto];
                 ?>
                 <td><?= ucfirst($nombreProducto) ?></td>
-                <td><?= $precioProducto ?>€</td>
-                <td><input type="number" name ="<?= $nombreProducto ?>" value="0" min="0" max="99"></td>
+                <td class="precio"><?= $precioProducto ?>€</td>
+                <td><input type="number" name ="<?= $nombreProducto ?>" value="<?= empty($_GET) ? 0 : $_GET[$nombreProducto] ?>" min="0" max="99"></td>
             </tr>
         <?php next($productos); } ?>
     </table>
     <input type="submit" value="Generar factura">
 <?php reset($productos); }
-
-    if (isset($_GET) && $_GET != null) {
-        generarFactura($productos);
-    }
-
     function generarFactura($productos) {
-        $productosCompra = array_intersect($_GET, $productos);
         $precioTotalGeneral = array();
-        // print_r($productosCompra);
-?>
-    <table>
-        <tr>
-            <th>Nombre</th>
-            <th>Cantidad</th>
-            <th>Precio unitario</th>
-            <th>Precio total</th>
-        </tr>
-        <?php for ($i = 0; $i < sizeof($productosCompra); $i++) { ?>
-            <tr>
-                <?php
-                    $nombre = key($productosCompra);
-                    $cantidad = $productosCompra[key($productosCompra)];
-                    $precioUnitario = $productos[key($productosCompra)];
-                    $precioTotal = $precioUnitario * $cantidad;
 
-                    array_push($precioTotalGeneral, $precioTotal);
-                ?>
-                <td><?= $nombre ?></td>
-                <td><?= $cantidad ?></td>
-                <td><?= $precioUnitario ?></td>
-                <td><?= $precioTotal ?></td>
+        $productosCompra = array();
+        foreach ($_GET as $producto => $cantidad) {
+            if ($cantidad != 0) {
+                $productosCompra[$producto] = $_GET[$producto];
+            }
+        }
+        if (!empty($productosCompra)) {
+?>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Cantidad</th>
+                <th>Precio unitario</th>
+                <th>Precio total</th>
             </tr>
-        <?php next($productosCompra); } ?>
-        <tr>
-            <th colspan="3">TOTAL</th>
-            <th><?= array_sum($precioTotalGeneral) ?></th>
-        </tr>
+            <?php for ($i = 0; $i < sizeof($productosCompra); $i++) {
+                $nombre = key($productosCompra);
+                $cantidad = $productosCompra[key($productosCompra)];
+                $precioUnitario = $productos[key($productosCompra)];
+                $precioTotal = $precioUnitario * $cantidad;
+
+                array_push($precioTotalGeneral, $precioTotal);
+
+                if (!empty($productosCompra[$nombre])) { ?>
+                <tr>
+                    <td><?= ucfirst($nombre) ?></td>
+                    <td class="precio"><?= $cantidad ?></td>
+                    <td class="precio"><?= $precioUnitario ?></td>
+                    <td class="precio"><?= $precioTotal ?></td>
+                </tr>
+                <?php } next($productosCompra); ?>
+            <?php } ?>
+            <tr>
+                <th colspan="3" class="precio">TOTAL</th>
+                <th class="precio"><?= array_sum($precioTotalGeneral) ?></th>
+            </tr>
+        </table>
+    <?php } ?>
 <?php } ?>
 
 <!DOCTYPE html>
@@ -78,11 +82,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LIsta de la compra</title>
+    <link rel="stylesheet" href="./css/main.css">
 </head>
 <body>
     <form action="enunciado.php" method="get">
         <?php imprimirLista($productos); ?>
+        <?php generarFactura($productos); ?>
     </form>
 </body>
 </html>
