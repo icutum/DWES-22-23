cc22238a84b55cda070dcc9bf717f718f31c532d
Mario <icutum@hotmail.com>
2022-10-08 23:18:47 +0200

    Casi terminado ejercicio 7, da error

--- Tema 2/enunciados/ejercicio7/ejercicio7.php
+++ Tema 2/enunciados/ejercicio7/ejercicio7.php
@@ -19,8 +19,45 @@
     foreach ($tareas as $tarea) {
         $lista[$tarea] = array_rand(array_flip($personas), 1);
     }
-
     print_r($lista);
+
+    ?> <br><br> <?php
+
+    // Organigrama
+    $dias = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+    $organigrama = array_flip($dias);
+
+    foreach ($organigrama as &$dia) {
+        $dia = array_flip($tareas);
+
+        foreach ($dia as &$persona) {
+            $persona = array_rand(array_flip($personas));
+        }
+    }
+    print_r($organigrama);
+
+    function imprimirOrganigrama($array) { ?>
+        <table border="1">
+            <tr>
+                <td><!-- Espacio en blanco --></td>
+                <?php for ($i = 0; $i < sizeof($array); $i++) : ?>
+                    <td><?= ucfirst(key($array)); next($array) ?></td>
+                <?php endfor; reset($array) ?>
+            </tr>
+
+            <?php $tarea = &$array[key($array)]; ?>
+            <?php for ($i = 0; $i < sizeof($tarea); $i++) : ?>
+                <tr>
+                    <td><?= ucfirst(key($tarea)); next($tarea) ?></td>
+
+                    <?php $persona = &$array ?>
+                    <?php foreach ($persona as &$minion) : ?>
+                        <td><?= $minion[key($minion)]; ?></td>
+                    <?php endforeach; reset($persona) ?>
+                </tr>
+            <?php endfor; reset($tarea) ?>
+        </table>
+    <?php }
 ?>
 
 <!DOCTYPE html>
@@ -32,6 +69,6 @@
     <title>Enunciado 7</title>
 </head>
 <body>
-    
+    <?= imprimirOrganigrama($organigrama); ?>
 </body>
 </html>
