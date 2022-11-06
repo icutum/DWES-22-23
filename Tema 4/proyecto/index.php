928d6577c4087c16e610fd53d95d0011fab60d35
Mario <icutum@hotmail.com>
2022-11-06 15:23:40 +0100

    Mejorado index del proyecto

--- Tema 4/proyecto/index.php
+++ Tema 4/proyecto/index.php
@@ -17,66 +17,68 @@
         <img class="header__logo" src="./img/tronco.png" alt="Tronco">
         <nav class="header__nav">
             <ul class="header__nav-list">
-                <li class="header__nav-item"><a class="header__nav-link" href="">Enlace innecesario</a></li>
-                <li class="header__nav-item"><a class="header__nav-link" href="">Enlace innecesario</a></li>
-                <li class="header__nav-item"><a class="header__nav-link" href="">Enlace innecesario</a></li>
+                <li class="header__nav-item"><a class="header__nav-link" href="">Pedro Sánchez</a></li>
+                <li class="header__nav-item"><a class="header__nav-link" href="">dame la</a></li>
+                <li class="header__nav-item"><a class="header__nav-link" href="">puta beca</a></li>
             </ul>
         </nav>
     </header>
     <main class="main">
         <form class="form" action="" method="post">
             <h2 class="form__title">Registrar alumno (pobre de él):</h2>
-            <fieldset class="form__fieldset">
-                <legend class="form__fieldset-title">Datos personales</legend>
-                <label class="form__label">
-                    Nombre:
-                    <input class="form__input" type="text" name="name" value="" placeholder="El nombre que te pusieron los engendros de tus padres">
-                </label>
-                <label class="form__label">
-                    Apellidos:
-                    <input class="form__input" type="text" name="surname" value="" placeholder="El primer apellido de cada uno de tus putos padres">
-                </label>
-                <label class="form__label">
-                    Sexo:
-                    <div class="form__radio">
-                        <label class="form__label">
-                            <input class="form__input form__input--radio" type="radio" name="gender" value="H"> Hombre
-                        </label>
-                        <label class="form__label">
-                            <input class="form__input form__input--radio" type="radio" name="gender" value="M"> Mujer
-                        </label>
-                        <label class="form__label">
-                            <input class="form__input form__input--radio" type="radio" name="gender" value="O" checked> Todos los días 
-                        </label>
-                    </div>
-                </label>
-                <label class="form__label">
-                    Fecha de nacimiento:
-                    <input class="form__input" type="date" name="birthdate">
-                </label>
-            </fieldset>
+            <div class="form__flex">
+                <fieldset class="form__fieldset">
+                    <legend class="form__fieldset-title">Datos personales</legend>
+                    <label class="form__label">
+                        Nombre:
+                        <input class="form__input" type="text" name="name" value="" placeholder="El nombre que te pusieron los engendros de tus padres">
+                    </label>
+                    <label class="form__label">
+                        Apellidos:
+                        <input class="form__input" type="text" name="surname" value="" placeholder="El primer apellido de cada uno de tus putos padres">
+                    </label>
+                    <label class="form__label">
+                        Sexo:
+                        <div class="form__radio">
+                            <label class="form__label">
+                                <input class="form__input form__input--radio" type="radio" name="gender" value="H"> Hombre
+                            </label>
+                            <label class="form__label">
+                                <input class="form__input form__input--radio" type="radio" name="gender" value="M"> Mujer
+                            </label>
+                            <label class="form__label">
+                                <input class="form__input form__input--radio" type="radio" name="gender" value="O" checked> Todos los días 
+                            </label>
+                        </div>
+                    </label>
+                    <label class="form__label">
+                        Fecha de nacimiento:
+                        <input class="form__input" type="date" name="birthdate">
+                    </label>
+                </fieldset>
 
-            <fieldset class="form__fieldset">
-                <legend class="form__fieldset-title">Datos de la cuenta</legend>
-                <label class="form__label">
-                    Usuario:
-                    <input class="form__input" type="text" name="user" value="" placeholder="El nombre de mierda con el que te van a identificar">
-                </label>
-                <label class="form__label">
-                    Contraseña:
-                    <input class="form__input" type="password" name="password" value="" placeholder="La contraseña de mierda que le vas a poner a la cuenta">
-                </label>
-                <label class="form__label">
-                    Correo:
-                    <input class="form__input" type="mail" name="mail" value="" placeholder="Tu puta dirección para saber a quién mandar spam">
-                </label>
-                <label class="form__label">
-                    Teléfono:
-                    <input class="form__input" type="number" name="telephone" value="" placeholder="Tu puto número para saber a quién llamar a la hora que más te joda">
-                </label>
-            </fieldset>
+                <fieldset class="form__fieldset">
+                    <legend class="form__fieldset-title">Datos de la cuenta</legend>
+                    <label class="form__label">
+                        Usuario:
+                        <input class="form__input" type="text" name="user" value="" placeholder="El nombre de mierda con el que te van a identificar">
+                    </label>
+                    <label class="form__label">
+                        Contraseña:
+                        <input class="form__input" type="password" name="password" value="" placeholder="La contraseña de mierda que le vas a poner a la cuenta">
+                    </label>
+                    <label class="form__label">
+                        Correo:
+                        <input class="form__input" type="mail" name="mail" value="" placeholder="Tu puta dirección para saber a quién mandar spam">
+                    </label>
+                    <label class="form__label">
+                        Teléfono:
+                        <input class="form__input" type="number" name="telephone" min="600000000" max="999999999" value="" placeholder="Tu puto número para saber a quién llamar a la hora que más te joda">
+                    </label>
+                </fieldset>
+            </div>
             
-            <input type="submit" value="Enviar" name="submit">
+            <input class="form__submit" type="submit" value="Enviar" name="submit">
         </form>
     </main>
 </body>
