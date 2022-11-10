bfc738b1fb5693bd791ca407087d9635ded89f4d
Mario <icutum@hotmail.com>
2022-11-10 16:35:19 +0100

    Separado header y nav en archivos aparte

---
+++ Tema 4/proyecto/header.php
@@ -0,0 +1,6 @@
+<header class="header">
+    <img class="header__logo" src="./img/tronco.png" alt="Tronco">
+    <nav class="header__nav">
+        <?php include_once("nav.php"); ?>
+    </nav>
+</header>
---
+++ Tema 4/proyecto/nav.php
@@ -0,0 +1,5 @@
+<ul class="header__nav-list">
+    <li class="header__nav-item"><a class="header__nav-link" href="">Pedro Sánchez</a></li>
+    <li class="header__nav-item"><a class="header__nav-link" href="">dame la</a></li>
+    <li class="header__nav-item"><a class="header__nav-link" href="">puta beca</a></li>
+</ul>
