660a282f21bbcea359410ae19f6a960a26ab7f23
Mario <icutum@hotmail.com>
2022-11-17 03:02:21 +0100

    Cambiado estilos generales

--- Tema 4/proyecto/nav.php
+++ Tema 4/proyecto/nav.php
@@ -1,3 +1,3 @@
 <ul class="header__nav-list">
-    <li class="header__nav-item"><a class="header__nav-link" href="./img/virgin-vs-chad.png">¿Por qué elegirnos?</a></li>
+    <li class="header__nav-item"><a class="header__nav-link" href="./img/virgin-vs-chad.png" target="_blank">¿Por qué elegirnos?</a></li>
 </ul>
