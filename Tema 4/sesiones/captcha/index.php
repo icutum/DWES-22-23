d84ec9493d349800a3309aafc467f11b0af97199
Mario <icutum@hotmail.com>
2022-12-17 13:32:19 +0100

    ¡FELICIDADES! ERES EL COMMIT Nº 100, RELLENA ESTE CAPTCHA

--- Tema 4/sesiones/captcha/index.php
+++ Tema 4/sesiones/captcha/index.php
@@ -1,5 +1,43 @@
 <?php
-    // Hacer un captcha que sea un texto
+    session_start();
+
+    const CAPTCHA_LENGTH = 4;
+    $captcha = &$_SESSION["captcha"];
+    $captcha = isset($captcha) ? $captcha : generateCaptcha(CAPTCHA_LENGTH);
+
+    function generateCaptcha($length) {
+        $str = "";
+
+        for ($i = 0; $i < CAPTCHA_LENGTH; $i++) {
+            $rand = rand(0, 1);
+
+            $rand > 0
+                ? $str .= rand(0, 9)
+                : $str .= chr(rand(65, 90));
+        }
+
+        return $str;
+    }
+
+    if (isset($_POST["submit"])) {
+        $guess = $_POST["captchaGuess"];
+
+        $captcha == $guess
+            ? header("Location: index.php?success")
+            : header("Location: index.php?failure");
+    }
+
+    function showMessage() {
+        switch (true) {
+            case isset($_GET["success"]) : ?>
+                <p>Has acertado el captcha</p>
+                <?php break;
+
+            case isset($_GET["failure"]): ?>
+                <p>Has fallado el captcha</p>
+                <?php break;
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +49,12 @@
     <title>Captcha</title>
 </head>
 <body>
-    
+    <h2>Captcha</h2>
+    <p><?= $captcha ?></p>
+    <p><?= showMessage() ?></p>
+    <form action="" method="post">
+        <input type="text" name="captchaGuess" value="">
+        <input type="submit" name="submit" value="Enviar">
+    </form>
 </body>
 </html>
