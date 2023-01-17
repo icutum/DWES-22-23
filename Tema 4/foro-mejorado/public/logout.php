00d01620588c2f88af051088590da2812cd65f5c
Mario <icutum@hotmail.com>
2023-01-17 21:13:39 +0100

    Barra de navegación Bootstrap

--- Tema 4/foro-mejorado/public/logout.php
+++ Tema 4/foro-mejorado/public/logout.php
@@ -2,4 +2,6 @@
     require_once("../src/init.php");
 
     session_destroy();
+
+    header("Location: listado.php");
 ?>
--- Tema 4/foro-mejorado/src/nav.php
+++ Tema 4/foro-mejorado/src/nav.php
@@ -1,30 +1,61 @@
 <?php
-    $nombre = isset($_SESSION["nombre"])
+    const ANON = "Anónimo";
+
+    $NOMBRE_USUARIO = isset($_SESSION["nombre"])
         ? $_SESSION["nombre"]
-        : "Anónimo"
+        : ANON
+    ;
+
+    $ID_USUARIO = $_SESSION["id"];
+
+    // Arrays clave-valor que contienen las páginas
+    // url => texto
+    $PAGINAS_ANON = [
+        "registro.php" => "Registrarse",
+        "login.php" => "Iniciar sesión",
+    ];
+
+    $PAGINAS_USUARIO = [
+        "detalle.php?id=$ID_USUARIO" => "Mi perfil",
+        "logout.php" => "Cerrar sesión"
+    ];
 ?>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+    <div class="container-fluid">
         <a class="navbar-brand" href="#"><?= $title ?></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse bg-secondary" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Listado</a>
+                    <a class="nav-link mx-5 active" aria-current="page" href="#">Listado</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><?= $nombre ?></a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
+                <?php
+                    if ($NOMBRE_USUARIO == ANON) :
+                        foreach ($PAGINAS_ANON as $url => $texto) : ?>
+                            <li class="nav-item">
+                                <a class="nav-link mx-5" href="<?= $url ?>"><?= $texto ?></a>
+                            </li>
+                        <?php endforeach;
+
+                    else : ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link mx-5 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?= $NOMBRE_USUARIO ?>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <?php foreach ($PAGINAS_USUARIO as $url => $texto) : ?>
+                                    <li><a class="dropdown-item" href="<?= $url ?>"><?= $texto ?></a></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </li>
+
+                    <?php endif;
+                ?>
             </ul>
         </div>
-    </div>
+	</div>
 </nav>
