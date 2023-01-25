e98fb0a5c9d4326afeec8005fc92147c8858142a
Mario <icutum@hotmail.com>
2023-01-25 18:37:08 +0100

    Arreglado recuperación contraseña

--- Tema 4/foro-mejorado/public/recuperar.php
+++ Tema 4/foro-mejorado/public/recuperar.php
@@ -6,18 +6,18 @@
         exit();
     }
 
+    // Enviar correo
     if (isset($_POST["submit-correo"])) {
         $token = bin2hex(openssl_random_pseudo_bytes($_ENV["TOKEN_LENGTH"]));
         $db->ejecuta(
-            "SELECT id FROM usuarios WHERE correo = ?",
+            "SELECT * FROM usuarios WHERE correo = ?",
             $_POST["correo"]
         );
-
         $usuario = $db->obtenElDato();
 
         if (isset($usuario)) {
             $db->ejecuta(
-                "INSERT INTO tokens (id_usuario, valor) VALUES (?, ?)",
+                "INSERT INTO tokens (id_usuario, valor, expiracion) VALUES (?, ?, (NOW() + INTERVAL 5 MINUTE))",
                 $usuario["id"], $token
             );
 
@@ -25,11 +25,30 @@
             Mailer::sendEmail(
                 $_POST["correo"],
                 "$title | Recuperación de contraseña",
-                "<a href='" . "$url" . "'>Pulse aquí para cambiar la contraseña</a>"
+                "<a target='_blank' href='" . "$url" . "'>Pulse aquí para cambiar la contraseña</a>"
             );
         }
 
-        header("Location: recuperar.php?sent-mail");
+        header("Location: recuperar.php?info=Se ha enviado el correo de recuperación de contraseña");
+    }
+
+    // Cambiar contraseña
+    if (isset($_GET["token"])) {
+        $db->ejecuta(
+            "SELECT * FROM tokens WHERE valor = ? AND expiracion > NOW()",
+            $_GET["token"]
+        );
+        $tokenValido = $db->obtenElDato();
+        $tokenValido = !empty($tokenValido);
+
+        if (!$tokenValido) {
+            $db->ejecuta(
+                "DELETE FROM tokens WHERE valor = ?",
+                $_GET["token"]
+            );
+
+            header("Location: recuperar.php?info=El token ha expirado");
+        }
     }
 
     if (isset($_POST["submit-contra"])) {
@@ -50,15 +69,16 @@
 
             header("Location: login.php");
         } else {
-            header("Location: recuperar.php?token=" . $_GET["token"] . "&error");
+            header("Location: recuperar.php?token=" . $_GET["token"] . "&error=Las contraseñas no coinciden");
         }
     }
 
     function formularioCorreo() { ?>
         <form action="" method="post" class="container mt-5 w-25">
-            <?php if (isset($_GET["sent-mail"])) : ?>
-                <p class="alert alert-info">Se ha enviado el correo de recuperación de contraseña</p>
+            <?php if (isset($_GET["info"])) : ?>
+                <p class="alert alert-info"><?= $_GET["info"] ?></p>
             <?php endif; ?>
+
             <h2 class="mb-3">Recuperar contraseña</h2>
             <div class="form-floating is-invalid mb-3">
                 <input type="mail" class="form-control" name="correo" placeholder="">
@@ -68,32 +88,24 @@
         </form>
     <?php }
 
-    $db->ejecuta("SELECT * FROM tokens WHERE valor = ?", $_GET["token"]);
-    $t = $db->obtenElDato();
+    function formularioContra() { ?>
+        <form action="" method="post" class="container mt-5 w-25">
+            <?php if (isset($_GET["error"])) : ?>
+                <p class="alert alert-danger"><?= $_GET["error"] ?></p>
+            <?php endif; ?>
 
-    function formularioContra($t) {
-
-        if (isset($t)) : ?>
-            <form action="" method="post" class="container mt-5 w-25">
-                <?php if (isset($_GET["error"])) : ?>
-                    <p class="alert alert-danger">Las contraseñas no coinciden</p>
-                <?php endif; ?>
-
-                <h2 class="mb-3">Recuperar contraseña</h2>
-                <div class="form-floating is-invalid mb-3">
-                    <input type="password" class="form-control" name="passwd" placeholder="">
-                    <label class="form-label">Nueva contraseña:</label>
-                </div>
-                <div class="form-floating is-invalid mb-3">
-                    <input type="password" class="form-control" name="reescribe-passwd" placeholder="">
-                    <label class="form-label">Vuelva a escribir la nueva contraseña:</label>
-                </div>
-                <input type="submit" class="w-100 btn btn-primary" name="submit-contra" value="Cambiar contraseña">
-            </form>
-        <?php else :
-            header("Location: recuperar.php");
-        endif;
-    }
+            <h2 class="mb-3">Recuperar contraseña</h2>
+            <div class="form-floating is-invalid mb-3">
+                <input type="password" class="form-control" name="passwd" placeholder="">
+                <label class="form-label">Nueva contraseña:</label>
+            </div>
+            <div class="form-floating is-invalid mb-3">
+                <input type="password" class="form-control" name="reescribe-passwd" placeholder="">
+                <label class="form-label">Vuelva a escribir la nueva contraseña:</label>
+            </div>
+            <input type="submit" class="w-100 btn btn-primary" name="submit-contra" value="Cambiar contraseña">
+        </form>
+    <?php }
 ?>
 
 <!DOCTYPE html>
@@ -104,10 +116,10 @@
 </head>
 <body>
     <?php require_once("../src/nav.php"); ?>
-    <?php if (!isset($_GET["token"])) {
-        formularioCorreo();
+    <?php if (isset($_GET["token"])) {
+        formularioContra();
     } else {
-        formularioContra($t);
+        formularioCorreo();
     } ?>
 </body>
 </html>
